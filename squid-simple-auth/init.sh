4deb0570064e7dbea3b43cb1d1dfc7159fd2c1b5
Marc Nuri <marc@marcnuri.com>
2024-05-30 12:14:09 +0200

    fix(squid-simple-auth): remove debug info

--- squid-simple-auth/init.sh
+++ squid-simple-auth/init.sh
@@ -3,7 +3,8 @@
 set -e
 
 htpasswd -bc /opt/squid/passwd "${SQUID_USERNAME}" "${SQUID_PASSWORD}"
-tail -f /opt/squid/access.log &
+tail -F /opt/squid/access.log /dev/null &
+tail -F /opt/squid/cache.log /dev/null &
 # Create missing cache directories and exit
 squid -Nz
 squid -f /etc/squid/squid.conf -NYC $*
